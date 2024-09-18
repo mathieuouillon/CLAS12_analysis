64fc8d421d7127af854da415c07d7553075ded1e
Mathieu Ouillon <mathieu.ouillon1@gmail.com>
2024-09-18 16:58:01 -0400

    add data to test

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,11 @@
 #include <hipo4/reader.h>
 
 auto main(int argc, char *argv[]) -> int {
-    std::cout << "Test" << std::endl;
+   std::string file = "../data/mydatafile.hipo";
+
+   hipo::reader   r(file.c_str());
+   hipo::banklist list = r.getBanks({"REC::Particle","REC::Event"});
+   int counter = 0;
+   while( r.next(list)&&counter<350){ counter++; list[0].show(); list[1].show();}
+
 }
