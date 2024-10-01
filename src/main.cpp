747b6751ce679bdf12d5191924f5f334b9476c43
Mathieu Ouillon <ouillon@jlab.org>
2024-10-01 10:56:26 -0400

    Add histogram from ROOT, fill it, and draw it.

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,32 @@
 #include <iostream>
 
+#include <TH1D.h>
+#include <TCanvas.h>
+
 #include <hipo4/reader.h>
 #include <thread_pool/BS_thread_pool.hpp>
 
 auto main(int argc, char *argv[]) -> int {
 
-   BS::thread_pool pool;
-     
-   std::string file = "../data/mydatafile.hipo";
+   auto hist = std::make_unique<TH1D>("hist","", 100, -20, 20);
 
-   hipo::reader   r(file.c_str());
-   hipo::banklist list = r.getBanks({"REC::Particle","REC::Event"});
-   int counter = 0;
-   while( r.next(list)&&counter<350){ counter++; list[0].show(); list[1].show();}
+  BS::thread_pool pool;
 
+  std::string file = "../data/mydatafile.hipo";
+
+  hipo::reader r(file.c_str());
+  hipo::banklist list = r.getBanks({"REC::Particle", "REC::Event"});
+  int counter = 0;
+  while (r.next(list) && counter < 350) {
+    counter++;
+    list[0].show();
+    list[1].show();
+for(int r = 0; r < list[0].getRows(); r++){
+    if(list[0].getInt("charge",r)!=0) hist->Fill(list[0].getFloat("vz",r));
+}
+  }
+
+  TCanvas canvas("", "", 800, 600);
+  hist->Draw();
+  canvas.SaveAs("hist.pdf");
 }
