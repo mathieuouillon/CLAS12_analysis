63e09d35c8713aa40d05bb2c06b087d6e93d6343
Mathieu Ouillon <ouillon@jlab.org>
2024-10-02 11:14:36 -0400

    Add fmt library

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,25 @@
+// std includes
 #include <iostream>
 
+// fmt includes
+#include <fmt/core.h>
+
+// ROOT includes
 #include <TCanvas.h>
 #include <TH1D.h>
 #include <TLine.h>
 #include <TROOT.h>
 
+// Core includes
 #include <hipo4/reader.h>
 #include <thread_pool/multi_thread.hpp>
 
+// Study 1 includes
 #include <study1/Reader.hpp>
 #include <study1/Histograms.hpp>
 
 int main(int argc, char* argv[]) {
+    fmt::println("Start program!");
     ROOT::EnableThreadSafety();  // To stop random errors in multithread mode
 
     std::vector<std::string> files = {"../data/mydatafile.hipo"};
